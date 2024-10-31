f889f3bc2c5bd6df8ef503454d8627898bc8a804
soojin <ssj1737@gmail.com>
2024-10-31 17:39:02 +0900

    1031

--- chap1/chap1/src/main/java/net/fullstack7/chap1/controller/BbsListController.java
+++ chap1/chap1/src/main/java/net/fullstack7/chap1/controller/BbsListController.java
@@ -1,15 +1,26 @@
 package net.fullstack7.chap1.controller;
 
+import net.fullstack7.chap1.dto.BbsDTO;
+import net.fullstack7.chap1.service.BbsService;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "/BbsListController", value = "bbs/list.do")
 
 public class BbsListController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        List<BbsDTO> bbsDTOList = BbsService.INSTANCE.getList();
+        req.setAttribute("bbsDTOList", bbsDTOList);
+
+        System.out.println("=====================================");
+        System.out.println("Bbs" +bbsDTOList);
+        System.out.println("=====================================");
+
         req.getRequestDispatcher("/WEB-INF/views/bbs/list.jsp").forward(req, res);
     }
 
--- chap1/chap1/src/main/java/net/fullstack7/chap1/service/BbsService.java
+++ chap1/chap1/src/main/java/net/fullstack7/chap1/service/BbsService.java
@@ -1,4 +1,32 @@
 package net.fullstack7.chap1.service;
 
+import net.fullstack7.chap1.dto.BbsDTO;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public enum BbsService {
+    INSTANCE;
+
+    public  List<BbsDTO> getList(){
+        List<BbsDTO> list = IntStream.range(1, 10).mapToObj(i->{
+            BbsDTO dto = new BbsDTO();
+            dto.setIdx(i);
+            dto.setBbsRefIdx(i);
+            dto.setBbsLevel((short) 1);
+            dto.setMemberId("user"+i);
+            dto.setTitle("테스트 제목 " + i);
+            dto.setContent("테스트 내용 " + i);
+            dto.setRegDate(LocalDateTime.now());
+            return dto;
+        }).collect(Collectors.toList());
+
+        System.out.println("=============================");
+        System.out.println(list);
+        System.out.println("=============================");
+        return list;
+    }
 }
