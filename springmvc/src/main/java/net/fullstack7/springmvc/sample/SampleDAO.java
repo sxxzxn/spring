ba3966f1a804f86ae1a81e9891876165a16a1e0b
soojin <ssj1737@gmail.com>
2024-11-06 15:05:19 +0900

    spring 시작

---
+++ springmvc/src/main/java/net/fullstack7/springmvc/sample/SampleDAO.java
@@ -0,0 +1,9 @@
+package net.fullstack7.springmvc.sample;
+
+import org.springframework.stereotype.Repository;
+
+//new~~와 같은 용도로 총네개가있음
+@Repository // 이건 데이터베이스 처리
+public class SampleDAO {
+    
+}
---
+++ springmvc/src/main/java/net/fullstack7/springmvc/sample/SampleService.java
@@ -0,0 +1,9 @@
+package net.fullstack7.springmvc.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service // 서비스용
+public class SampleService {
+    private SampleDAO sampleDAO;
+
+}
